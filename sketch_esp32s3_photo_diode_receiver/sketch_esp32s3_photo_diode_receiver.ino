26b31a1a6f0cc1df8a1a022617a7d9fb95db103d
mukuldeep maiti <mukul0018@gmail.com>
2025-06-14 02:55:38 +0530

    Working code for laser communication using photodiode and laser module on ESP32-S3 s show the information transmitted and received on the screen

--- sketch_esp32s3_photo_diode_receiver/sketch_esp32s3_photo_diode_receiver.ino
+++ sketch_esp32s3_photo_diode_receiver/sketch_esp32s3_photo_diode_receiver.ino
@@ -1,11 +1,41 @@
+#include <TFT_eSPI.h>
+#include <SPI.h>
+
+TFT_eSPI tft = TFT_eSPI();
+
+#define BUFFER_SIZE 12
+char lastChars[BUFFER_SIZE];  // Circular buffer
+int indexG = 0; 
+
 // Photodiode Receiver (ESP32-S3)
 const int PHOTODIODE_PIN = 3;
-const int threshold = 1800; // Adjust based on ambient light
-const int bitDelay = 1000;  // Microseconds (must match transmitter)
+const int threshold = 4000; // Adjust based on ambient light
+const int bitDelay = 250000;  // Microseconds (must match transmitter)
+char zero = 0;
+
+
 
 void setup() {
   Serial.begin(115200);
   Serial.println("starting");
+
+  tft.init();
+  tft.setRotation(1);  // 0–3; try different values if display is upside down
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextSize(4);
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+
+  tft.setCursor(20, 0);
+  tft.println("# RECEIVER #");
+  
+  tft.setCursor(0, 60);
+  tft.println("booting up...");
+
+  for (int i = 0; i < BUFFER_SIZE; i++) {
+    lastChars[i] = ' ';
+  }
+
+
 }
 
 bool readBit() {
@@ -14,7 +44,7 @@ bool readBit() {
 }
 
 char readByte() {
-  while (readBit()); // Wait for start bit (LOW)
+  while (!readBit()); // Wait for start bit (HIGH)
   delayMicroseconds(bitDelay);
 
   char c = 0;
@@ -26,15 +56,41 @@ char readByte() {
 
   // Wait for stop bit
   delayMicroseconds(bitDelay);
+
   return c;
 }
 
 void loop() {
-  //char received = readByte();
-  //Serial.print(received);
-  int value = analogRead(PHOTODIODE_PIN);
-  Serial.println(value);
+  char received = readByte();
+  
+  if(received != zero){
+    //Serial.println(received);
 
-  delay(50);
+    lastChars[indexG] = received;
+    indexG = (indexG + 1) % BUFFER_SIZE;
+
+    // Print full buffer
+    tft.fillRect(0, 60, 320, 60, TFT_BLACK);
+    //tft.fillScreen(TFT_BLACK);
+    tft.setCursor(0, 60);
+
+    Serial.print("Last 10 chars: ");
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+      int idx = (indexG + i) % BUFFER_SIZE;
+      Serial.print(lastChars[idx]);
+      tft.print(lastChars[idx]);
+    }
+    Serial.println();
+  }
+
+  // int r_int = received;
+  // if( r_int > 0){
+  //   Serial.println(received);
+  // }
+
+  // int value = analogRead(PHOTODIODE_PIN);
+  // Serial.println(value);
+
+  // delay(50);
 
 }
--- sketch_esp32s3_photo_diode_transmitter/sketch_esp32s3_photo_diode_transmitter.ino
+++ sketch_esp32s3_photo_diode_transmitter/sketch_esp32s3_photo_diode_transmitter.ino
@@ -1,17 +1,41 @@
+#include <TFT_eSPI.h>
+#include <SPI.h>
+
+TFT_eSPI tft = TFT_eSPI();
+
+#define BUFFER_SIZE 12
+char lastChars[BUFFER_SIZE];  // Circular buffer
+int indexG = 0; 
+
 // Laser Transmitter (ESP32-S3)
 const int LASER_PIN = 44;
-const char* message = "Hello via LASER!";
-const int bitDelay = 1000; // Microseconds per bit
+const char* message = " Hello World!   SUBSCRIBE to DIY Days :) ";
+const int bitDelay = 250000; // Microseconds per bit
+//const int byteDelay = 1000; // Microseconds per byte
+
+
 
 void setup() {
   pinMode(LASER_PIN, OUTPUT);
   Serial.begin(115200);
   Serial.println("starting");
+
+  tft.init();
+  tft.setRotation(1);  // 0–3; try different values if display is upside down
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextSize(4);
+  tft.setTextColor(TFT_CYAN, TFT_BLACK);
+  tft.setCursor(20, 0);
+  tft.println("# SENDER #");
+  
+  tft.setCursor(0, 60);
+  tft.println("booting up...");
+
 }
 
 void sendByte(char c) {
   // Start bit
-  digitalWrite(LASER_PIN, LOW);
+  digitalWrite(LASER_PIN, HIGH);
   delayMicroseconds(bitDelay);
 
   // Send each bit LSB first
@@ -22,20 +46,45 @@ void sendByte(char c) {
   }
 
   // Stop bit
-  digitalWrite(LASER_PIN, HIGH);
+  digitalWrite(LASER_PIN, LOW);
   delayMicroseconds(bitDelay);
+  //delayMicroseconds(byteDelay);
+
 }
 
 void loop() {
 
   Serial.println("sending ");
 
-  // for (int i = 0; message[i]; i++) {
-  //   sendByte(message[i]);
-  // }
-  digitalWrite(LASER_PIN,HIGH);
-  delay(2000); // Wait and send again
-  digitalWrite(LASER_PIN, LOW);
-  delay(2000);
+  for (int i = 0; message[i]; i++) {
+    sendByte(message[i]);
+    Serial.println(message[i]);
+
+    //to sync 
+    lastChars[indexG] = message[i];
+    indexG = (indexG + 1) % BUFFER_SIZE;
+
+    // Print full buffer
+    //tft.fillScreen(TFT_BLACK);
+    tft.fillRect(0, 60, 320, 60, TFT_BLACK);
+    tft.setCursor(0, 60);
+
+    Serial.print("Last 10 chars: ");
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+      int idx = (indexG + i) % BUFFER_SIZE;
+      Serial.print(lastChars[idx]);
+      tft.print(lastChars[idx]);
+    }
+    Serial.println();
+
+
+  }
+  delay(5000);
+
+
+  // digitalWrite(LASER_PIN,HIGH);
+  // delay(2000); // Wait and send again
+  // digitalWrite(LASER_PIN, LOW);
+  // delay(2000);
 
 }
